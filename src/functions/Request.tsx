b981be719342ce6047e76da1c9f55b5a8b8864e6
DovCaio <onlydeadnotalive@gmail.com>
2024-07-27 17:38:13 -0300

    Refatorado uma parte míninma

--- src/functions/Request.tsx
+++ src/functions/Request.tsx
@@ -1,31 +1,35 @@
 
 import Person from "@/model/Person"
-import axios from "axios"
+import axios, { AxiosResponse } from "axios"
 
 import ResponseBox from "@/components/templates/ResponseBox"
+
 
 export default class Request {
 
     private url: string = "http://localhost:8080/persons"
+    
+    private sucess(setRequestBox: Function, response: AxiosResponse<any, any>){
 
+        setRequestBox(
+            <ResponseBox text={response.status} goodRequest={true} setRequestBox={setRequestBox}/>
+        )
+
+    }
+
+    private fail(setRequestBox: Function, err: any){
+
+        setRequestBox(
+            <ResponseBox text={err.message} goodRequest={false} setRequestBox={setRequestBox}/>
+        )
+
+    }
 
     public async post(person: Person, setRequestBox: Function){
         
         return await axios.post(this.url, person)
-                        .then((response) =>{
-
-                            setRequestBox(
-                                <ResponseBox text={response.status} goodRequest={true} setRequestBox={setRequestBox}/>
-                            )
-
-                        })
-                        .catch((err) => {
-
-                            setRequestBox(
-                                <ResponseBox text={err.message} goodRequest={false} setRequestBox={setRequestBox}/>
-                            )
-
-                        })
+                        .then((response) => this.sucess(setRequestBox, response))
+                        .catch((err) => this.fail(setRequestBox, err))
                         
 
     }
